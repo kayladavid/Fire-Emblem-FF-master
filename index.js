7cbb7092b63e86f5c100cd569cc7d1cdf8e2eae3
kayladavid <56418112+kayladavid@users.noreply.github.com>
2020-09-14 11:11:12 -0700

    update

--- index.js
+++ index.js
@@ -7,11 +7,22 @@ form.addEventListener("submit", function(){
 
     // Only when the form has been submitted do you want the textbox value
     let inputTest = document.getElementById('story').value;
-    localStorage.setItem( 'objectToPass', JSON.stringify(inputTest));
+    localStorage.setItem( 'myObject', JSON.stringify(inputTest));
 
     var myObject = JSON.parse(localStorage.getItem('myObject'));
 
-    window.location.href = 'display.html';
+    window.location.href = 'display.html'.log;
 
-  });
+  }); 
+
+
+  /*
+  function createLink(val) {
+    document.getElementById("link").innerHTML = val;
+    document.getElementById('button').style.display = 'none';
+    document.getElementById('story').style.display = 'none';
+
+    localStorage.setItem("textArea", val);
+} */
+
 
