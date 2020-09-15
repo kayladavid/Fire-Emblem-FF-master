e86d6c5bb6e49e944352a2deeabd5215eb226987
kayladavid <56418112+kayladavid@users.noreply.github.com>
2020-09-15 10:03:54 -0700

    update

--- display.js
+++ display.js
@@ -1,6 +1,14 @@
+let nameHere = localStorage['nameToPass'];
+let headHere = localStorage['titleToPass'];
 let inputTest = localStorage['objectToPass'];
+
+      localStorage.removeItem( 'nameToPass' );
+      localStorage.removeItem( 'titleToPass' );
       localStorage.removeItem( 'objectToPass' ); // Clear the localStorage
       
+      document.getElementById("user").innerHTML = nameHere;
+      document.getElementById("head").innerHTML = headHere;
       document.getElementById("container").innerHTML= inputTest;
-     // alert('Inserted Data' + inputTest);
+     
+      
 
--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 // console.log ('Hello world');
-
+let name = document.getElementById('uname');
+let title = document.getElementById('title');
 let inputTest = document.getElementById('story');
 
 document.getElementById('buttonId').addEventListener('click',store_data);  //Button Click Listener.
 
 function store_data(){ // Store value from input field to local store
+    localStorage.setItem('nameToPass', name.value);
+    localStorage.setItem('titleToPass', title.value);
     localStorage.setItem( 'objectToPass', inputTest.value);
 }
 
 
+// save name and title into card, save data each time, responsive.
 
 
 
