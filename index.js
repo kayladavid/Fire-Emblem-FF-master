553f57bad9dfb17163b18c765e6cd8697602521d
kayladavid <56418112+kayladavid@users.noreply.github.com>
2020-09-18 08:43:06 -0700

    update

--- index.js
+++ index.js
@@ -6,14 +6,28 @@ const storyInput = document.querySelector('.input');
 
 function template(data) {
   commentList.insertAdjacentHTML("beforeend", `
-  <p class="story-body">${data.comment}</p>`);
+  <div class="comment flex items-start justify-start">
+  <p class="time">${dateString}</p>
+  <div class="flex-1">
+    <p class="story-body">${data.comment}</p>
+  </div>
+</div>
+</div>`);
 }
+
+const currentDate = new Date();
+
+const date = currentDate.getDate();
+const month = currentDate.getMonth(); //Be careful! January is 0 not 1
+const year = currentDate.getFullYear();
+
+const dateString = date + "-" +(month + 1) + "-" + year;
 
 function appendComment (event) {
 
   const data = {
-    comment: storyInput.value,
-  };
+    comment: storyInput.value
+    };
 
   event.preventDefault();
 
